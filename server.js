012f2697ed45d7dc38d1abbffa4cd7d4ad0a3cb4
Weppman <joweppelman@gmail.com>
2025-04-10 21:58:46 +0200

    Incorrect port in the file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // Serve static files from React build
 app.use(express.static(path.join(__dirname, 'build')));
