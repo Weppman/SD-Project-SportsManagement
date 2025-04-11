adcf68d9d81592547b8294ea41ad705cc04a9aca
Weppman <75846543+Weppman@users.noreply.github.com>
2025-04-11 18:58:39 +0200

    Update App.js
    Adding Form Linking

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import IssuePage from './Issue page and assoicated stuff/Issues'; // optional if you have a homepage
 import TestPage from './MainUIComponents/testMainUI';
+import BookingPage from './Bookings/bookingForm';
 
 function App() {
   return (
@@ -10,12 +11,14 @@ function App() {
         <ul>
           <li><Link to="/">Issue Page</Link></li>
           <li><Link to="/test">Test Page</Link></li>
+          <li><link to="/bookings">Bookings Page</link></li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<IssuePage />} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="/bookings" element={<BookingPage /> />
       </Routes>
     </Router>
   );
