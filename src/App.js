52f757d31544b064cc8b7b3a120145e7f6cb65bd
Weppman <75846543+Weppman@users.noreply.github.com>
2025-04-11 18:37:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
+// src/pages/TestPage.js (renaming from test.ui.js is optional but cleaner)
 import React, { useState } from 'react';
-import './MainUIComponents/style.css';
+import '../MainUIComponents/style.css';
 
-
-function App() {
-  
+const TestPage = () => {
   const [buttonText, setButtonText] = useState("Click me");
-  
+
   const getButtonHandlers = () => {
     const handleClick = () => alert("Clicked!");
     const handleHover = () => setButtonText("Hovering");
-  
-    return { handleClick, handleHover  };
+
+    return { handleClick, handleHover };
   };
 
-
-  // 2. Destructure what you need
   const { handleClick, handleHover } = getButtonHandlers();
-  
+
   return (
-    
     <section>
       <h1>Test</h1>
-      <button onClick={handleClick}
+      <button
+        onClick={handleClick}
         onMouseEnter={handleHover}
         onMouseLeave={() => setButtonText('Interact Me')}
-        >
-      {buttonText} 
-    </button>
+      >
+        {buttonText}
+      </button>
     </section>
   );
-}
+};
 
-export default App;
+export default TestPage;
