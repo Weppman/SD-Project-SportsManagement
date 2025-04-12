5199f8471edbb6910fdd819afa04059eb01f1792
Weppman <joweppelman@gmail.com>
2025-04-12 13:59:19 +0200

    Added changes to secure API keys for Firebase and Google Maps in the .env file.

--- src/Firebase/firebaseApp.js
+++ src/Firebase/firebaseApp.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCtnpp9hKaS5XcX96JUuUCPV1FS5EKU3Rg",
-    authDomain: "sportsfacility-ce031.firebaseapp.com",
-    projectId: "sportsfacility-ce031",
-    storageBucket: "sportsfacility-ce031.firebasestorage.app",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID",
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
